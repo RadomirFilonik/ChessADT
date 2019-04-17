c4c4ff703b51c263f74c32792dca2fb54b37ab79
Radek Filonik <rfilonik@interia.pl>
2019-04-17 18:57:10 +0200

    aaa

--- ChessADT/App_Start/RouteConfig.cs
+++ ChessADT/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ChessADT
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ChessBoard", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
